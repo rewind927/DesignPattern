2a592637b4592b660b3934470cb840d2e47bcf93
rewind927 <rewind927@gmail.com>
2015-01-03 23:28:36 +0800

    create turkey and duck example in adapter pattern

---
+++ src/com/design/pattern/adapter/MallardDuck.java
@@ -0,0 +1,11 @@
+package com.design.pattern.adapter;
+
+public class MallardDuck implements Duck {
+	public void quack() {
+		System.out.println("Quack");
+	}
+ 
+	public void fly() {
+		System.out.println("I'm flying");
+	}
+}
