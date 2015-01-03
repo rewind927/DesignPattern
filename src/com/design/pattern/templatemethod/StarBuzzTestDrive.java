0cd86cf230d08afb73444f551166db86ad9ad3d4
rewind927 <rewind927@gmail.com>
2015-01-03 18:47:45 +0800

    create example of template method pattern

---
+++ src/com/design/pattern/templatemethod/StarBuzzTestDrive.java
@@ -0,0 +1,16 @@
+package com.design.pattern.templatemethod;
+
+public class StarBuzzTestDrive {
+	public static void main(String[] args) {
+ 
+		Tea tea = new Tea();
+		Coffee coffee = new Coffee();
+ 
+		System.out.println("\nMaking tea...");
+		tea.prepareRecipe();
+ 
+		System.out.println("\nMaking coffee...");
+		coffee.prepareRecipe();
+
+	}
+}
