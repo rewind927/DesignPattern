63eba65e9f808cb0a758cab09e3a72ce16a2a179
rewind927 <rewind927@gmail.com>
2015-01-11 23:33:13 +0800

    fix naming error

--- src/com/design/pattern/decorator/BurgerQueen.java
+++ src/com/design/pattern/decorator/BurgerQueen.java
@@ -4,17 +4,17 @@ public class BurgerQueen {
 
     public static void main(String[] args) {
         
-        Burger beefBurger = new SubmarineSandwich();
-        System.out.println(beefBurger.getDescription());
-        System.out.println(beefBurger.cost()+"\n");
+        Burger submarineSandwich = new SubmarineSandwich();
+        System.out.println(submarineSandwich.getDescription());
+        System.out.println(submarineSandwich.cost()+"\n");
         
-        Burger baconBurger = new Sandwitch();
-        System.out.println(baconBurger.getDescription());
-        System.out.println(baconBurger.cost()+"\n");
+        Burger sandwitch = new Sandwitch();
+        System.out.println(sandwitch.getDescription());
+        System.out.println(sandwitch.cost()+"\n");
         
-        Burger lettuceBurger = new Whopper();
-        System.out.println(lettuceBurger.getDescription());
-        System.out.println(lettuceBurger.cost()+"\n");
+        Burger whopper = new Whopper();
+        System.out.println(whopper.getDescription());
+        System.out.println(whopper.cost()+"\n");
     }
 
 }
