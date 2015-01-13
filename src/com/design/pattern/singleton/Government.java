9323880f1ad5c6383bf5408fee3c06af36b6751c
rewind927 <rewind927@gmail.com>
2015-01-14 00:19:45 +0800

    add singleton example

---
+++ src/com/design/pattern/singleton/Government.java
@@ -0,0 +1,13 @@
+package com.design.pattern.singleton;
+
+public class Government {
+    private int officer;
+
+    public void setOfficer(int officer) {
+        this.officer = officer;
+    }
+
+    public int getOfficer() {
+        return officer;
+    }
+}
