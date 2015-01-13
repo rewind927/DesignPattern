edbd9eedfa38a005e7feb2170ef3faa71be4cf16
rewind927 <rewind927@gmail.com>
2015-01-14 00:41:47 +0800

    add example for practice singleton pattern

--- src/com/design/pattern/singleton/Government.java
+++ src/com/design/pattern/singleton/Government.java
@@ -3,6 +3,11 @@ package com.design.pattern.singleton;
 public class Government {
     private int officer;
 
+    public static Government getInstance() {
+        // TODO refactor with singleton pattern and add thread safe mechanism
+        return new Government();
+    }
+
     public void setOfficer(int officer) {
         this.officer = officer;
     }
