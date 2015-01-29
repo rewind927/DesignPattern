8375d40029ceca35251bd04b82e22bd758768e19
rewind927 <rewind927@gmail.com>
2015-01-29 21:30:49 +0800

    Add an example for chain of responsibility pattern.

---
+++ src/com/design/pattern/chain/OrcCommander.java
@@ -0,0 +1,16 @@
+package com.design.pattern.chain;
+
+public class OrcCommander {
+    public void handleRequest(Request request) {
+        printHandling(request);
+    }
+
+    protected void printHandling(Request request) {
+        System.out.println(this + " handling request \"" + request + "\"");
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName();
+    }
+}
