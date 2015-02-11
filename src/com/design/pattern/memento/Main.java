ceec83d919783ef6b95bc4ccd8c1e4eb7a1f048d
ethanshen <ethanshen@kkbox.com>
2015-02-12 02:21:47 +0800

    Modify for practice

--- src/com/design/pattern/memento/Main.java
+++ src/com/design/pattern/memento/Main.java
@@ -13,12 +13,12 @@ public class Main {
 		Backup backup = application.backup();  // 建立備忘
 		recovery.add(backup); // 加入備忘錄
 
-		application.setState("customer setting");
+		application.setState("customer setting"); // 修改備忘錄
 		System.out.println(application.getState());
 
-		Date date = backup.getDate(); // 假設 date 是使用者自行設定所要取得的還原時間！
-		application.recover(recovery.retrieve(date)); // 取得備忘來還原
-		System.out.println(application.getState());
+		// TODO: 還原備忘錄（假設 date 是使用者自行設定所要取得的還原時間）
+		Date date = backup.getDate();
+
 	}
 
 }
