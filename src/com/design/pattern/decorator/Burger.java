4898bc1b73222152d0126ad6b68dd04a9fca4b06
ethanshen <ethanshen@kkbox.com>
2015-02-11 14:25:17 +0800

    Refactor decorator design pattern

--- src/com/design/pattern/decorator/Burger.java
+++ src/com/design/pattern/decorator/Burger.java
@@ -1,6 +1,6 @@
 package com.design.pattern.decorator;
 
 public interface Burger {
-    public abstract String getDescription();
-    public abstract float cost();
+    public String getDescription();
+    public float cost();
 }
