5983fc8b5d613d1159152722e81000dfa646593b
Kami <kamichen@kkbox.com>
2015-03-12 16:47:15 +0800

    Refactor bridge pattern demo

--- src/com/design/pattern/bridge/Factory.java
+++ src/com/design/pattern/bridge/Factory.java
@@ -1,5 +1,6 @@
 package com.design.pattern.bridge;
 
+import com.design.pattern.bridge.drawing.Drawing;
 import com.design.pattern.bridge.drawing.V1Drawing;
 import com.design.pattern.bridge.drawing.V2Drawing;
 import com.design.pattern.bridge.shape.Circle;
@@ -17,10 +18,13 @@ public class Factory {
     }
 
     public void create() {
-        shapes.add(new Rectangle(new V1Drawing(), 2, 2, 5, 5));
-        shapes.add(new Circle(new V1Drawing(), 2, 2, 5));
-        shapes.add(new Rectangle(new V2Drawing(), 1, 1, 7, 7));
-        shapes.add(new Circle(new V2Drawing(), 3, 3, 6));
+        Drawing v1Drawing = new V1Drawing();
+        Drawing v2Drawing = new V2Drawing();
+
+        shapes.add(new Rectangle(v1Drawing, 2, 2, 5, 5));
+        shapes.add(new Circle(v1Drawing, 2, 2, 5));
+        shapes.add(new Rectangle(v2Drawing, 1, 1, 7, 7));
+        shapes.add(new Circle(v2Drawing, 3, 3, 6));
     }
 
     public List<Shape> getShapes() {
