994a1ca035fda6fa1c3feb4a160f350fb9dbf107
ZoeyHuang <zoeyhuang@kkbox.com>
2015-03-18 23:45:16 +0800

    Add compositeClient

---
+++ src/com/design/pattern/composite/CompositeClient.java
@@ -0,0 +1,19 @@
+package com.design.pattern.composite;
+
+public class CompositeClient {
+    public static StringBuffer g_indent = new StringBuffer();
+
+    public static void main(String[] args) {
+        Employee john = new Developer("John", "Developer");
+        Employee mary = new Developer("Mary", "Developer");
+//        Employee pm = new Manager("Bob", "PM");
+//        pm.add(john);
+//        pm.add(mary);
+
+        Employee jason = new Developer("Michael", "Sales");
+        Manager generalManager = new Manager("BigPerson", "CEO");
+//        generalManager.add(jason);
+//        generalManager.add(pm);
+//        generalManager.print();
+    }
+}
