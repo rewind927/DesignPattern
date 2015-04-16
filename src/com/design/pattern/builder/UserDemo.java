a2e4e98b4863b89a06e52a9f4b110151a0e46513
KarneLin <karenlin@kkbox.com>
2015-04-17 00:02:35 +0800

    Init builder pattern.

---
+++ src/com/design/pattern/builder/UserDemo.java
@@ -0,0 +1,18 @@
+package com.design.pattern.builder;
+
+public class UserDemo {
+    public static void main(String[] args) {
+
+        User userKaren = new User.UserBuilder("Karen", "Lin").build();
+
+        System.out.println("User's first Name: " + userKaren.getFirstName());
+        System.out.println("User's last Name: " + userKaren.getLastName());
+        System.out.println("User's age: " + userKaren.getAge());
+        System.out.println("User's phone: " + userKaren.getPhone());
+        System.out.println("User's address: " + userKaren.getAddress() + "\n");
+
+        
+        //TODO User: Grass Dig, Age: 18, Phone: 7533967, Address: Taipei
+
+    }
+}
